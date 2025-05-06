a46e62aac856c67f6ca907305231a10a8082a069
adr <andres@duarterengifo.com>
2025-05-06 13:40:43 -0400

    make join more polymorphic

--- packages/domain/src/functions/i-z-set/binary/join.ts
+++ packages/domain/src/functions/i-z-set/binary/join.ts
@@ -7,16 +7,16 @@ import { mapInternal } from "../abstractions/map-internal.js"
 import { mulInternal } from "./mul.js"
 
 export const join =
-  <Key, Data, W>(ring: Ring<W>) =>
-  (fn: (x: Data, y: Data) => Data) =>
-  (other: IZSet<Key, Data, W>) =>
-  (self: IZSet<Key, Data, W>): IZSet<Key, Data, W> =>
+  <Key, D0, D1, D2, W>(ring: Ring<W>) =>
+  (fn: (x: D0, y: D1) => D2) =>
+  (other: IZSet<Key, D1, W>) =>
+  (self: IZSet<Key, D0, W>): IZSet<Key, D2, W> =>
     pipe(
       self,
       mapInternal((self) =>
         pipe(
-          mulInternal<Data, Data, Data, W>(ring)(fn),
-          mergePerfect<HM.HashMap<Data, W>, HM.HashMap<Data, W>, HM.HashMap<Data, W>, Key, Key>
+          mulInternal<D0, D1, D2, W>(ring)(fn),
+          mergePerfect<HM.HashMap<D0, W>, HM.HashMap<D1, W>, HM.HashMap<D2, W>, Key, Key>
         )(other.index)(self)
       )
     )
