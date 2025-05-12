304db65c069f67d2a6570b6704d5dab330302255
adr <andres@duarterengifo.com>
2025-05-12 11:23:00 -0400

    save but nothing works anyway.

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['@a33/tdbsp']
-  }
+    include: ['@a33/tdbsp']
+  },
+    server: {
+    watch: {
+      // Optional: If HMR for linked package is an issue, you might investigate
+      // watching the build output of the linked package.
+      ignored: ['!**/node_modules/a33/tdbsp/build/**'],
+    },
+  },
 })
