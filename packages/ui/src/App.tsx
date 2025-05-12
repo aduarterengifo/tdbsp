c3fe12f303958c66f3ea91b239b52890fb23de2c
adr <andres@duarterengifo.com>
2025-05-12 13:20:43 -0400

    somehow it works now, but it took everything

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { floppy } from '@a33/tdbsp/src/floppy'
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log('floppy', floppy)
   return (
     <>
       <div>
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@a33/tdbsp']
-  },
-    server: {
-    watch: {
-      // Optional: If HMR for linked package is an issue, you might investigate
-      // watching the build output of the linked package.
-      ignored: ['!**/node_modules/a33/tdbsp/build/**'],
+  resolve: {
+    alias: {
+      '@a33/tdbsp': path.resolve(__dirname, '../tdbsp'), 
     },
   },
 })
